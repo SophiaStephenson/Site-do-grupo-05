d0c8b1b2bcf7e6521bcd19e087550d7feb2769ca
Sophia Stephenson <104010449+SophiaStephenson@users.noreply.github.com>
2023-09-18 12:07:01 -0300

    4

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ function tocaSomClap (idElementoAudio) {
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let Contador = 0;
-
 while (Contador < ListaDeTeclas.length) {
 ListaDeTeclas[Contador].onclick = function () {
   tocaSomClap('#som-tecla-toim');
 };
+const vitorroque = ListaDeTeclas[Contador].classList[1];
+console.log(vitorroque);
 Contador = Contador + 1;
 console.log(Contador);
 }
