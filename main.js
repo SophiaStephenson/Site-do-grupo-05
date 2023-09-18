db3848901c820c5fde0a24168de6da4956036e68
Sophia Stephenson <104010449+SophiaStephenson@users.noreply.github.com>
2023-09-18 11:42:23 -0300

    oiiii

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < ListaDeTeclas.length) {
-ListaDeTeclas[Contador].onclick = tocar('#som_tecla_toim');
+ListaDeTeclas[Contador].onclick = tocar('#som-tecla-toim');
 Contador = Contador + 1;
 console.log(Contador);
 }
