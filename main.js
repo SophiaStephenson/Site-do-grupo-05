fab94aec29051db7e377f6cf1821c9a646571a2c
Sophia Stephenson <104010449+SophiaStephenson@users.noreply.github.com>
2023-09-25 11:34:48 -0300

    tygf

--- main.js
+++ main.js
@@ -7,13 +7,20 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 while (Contador < ListaDeTeclas.length) {
-ListaDeTeclas[Contador].onclick = function () {
+ListaDeTeclas[Contador].onclick = function () 
+console.log(vitorroque);
+
+const vitorroque= `som_${classe}`;
+console.log(vitorroque);
+
+{
   tocaSomClap('#som-tecla-toim');
 };
-const vitorroque= 'som_${classe}';
+
+const vitorroque2= `som_${classe}`;
 const vitorroque = ListaDeTeclas[Contador].classList[1];
 console.log(vitorroque);
-console.log(vitorroque)
+
 Contador = Contador + 1;
 console.log(Contador);
 }
